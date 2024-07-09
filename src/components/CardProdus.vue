3ad99f2a5a7c46761130f60c92a4ad1845460bc6
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-07-10 01:34:00 +0300

    card store template

---
+++ src/components/CardProdus.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+<script>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "CardProdus",
+    props: {
+        id: Number,
+        image: String,
+        title: String,
+        description: String,
+        price: Number,
+        discount: Number,
+        available: Boolean,
+        
+    },
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.id);
+        }
+    }
+});
+</script>
+<style lang=""></style>
