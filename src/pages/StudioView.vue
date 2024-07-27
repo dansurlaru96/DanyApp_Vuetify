16f74c3e41a01e8fb3d07175813e31e275231619
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-07-28 00:10:05 +0300

    refactor: Added accordion panels in Studio page

--- src/pages/StudioView.vue
+++ src/pages/StudioView.vue
@@ -2,69 +2,149 @@
   <v-parallax
     height="300"
     src="../assets/mixer_parallax-page.jpg"
-    class="low-contrast-image">
-  <div class=" text-center text-white ma-15">
-    <h1 class="text-h1">Studio</h1>
-    <p class="text-h5">Transformă-ți camera în studio de înregistrări cu costuri minime</p>
-  </div>
+    class="low-contrast-image"
+  >
+    <div class="text-center text-white ma-15">
+      <h1 class="text-h1">Studio</h1>
+      <p class="text-h5">
+        Transformă-ți camera în studio de înregistrări cu costuri minime
+      </p>
+    </div>
   </v-parallax>
 
   <div class="ma-4">
     <p>
-      Dacă ai ajuns să citești aceste rânduri, înseamnă că ești un pasionat de muzică și îți dorești să te vezi într-un studio de înregistrări, în care muzica ta are o claritate aparte și devine un produs finit, pregătit să ajungă la urechile publicului.
-      <br>
-      <br>
-      Ei bine, eu sunt aici pentru a-ți spune că <b> poți avea propriul tău home studio, la tine acasă!</b> Cu un buget minim și cu puțină creativitate, poți transforma o cameră din locuința ta într-un studio de înregistrări.
-      <br>
-      <br>
-      În acest articol, îți voi prezenta pașii pe care trebuie să îi urmezi pentru a-ți construi propriul studio de înregistrări, la tine acasă. Vei vedea că nu este nevoie de investiții majore, ci doar de puțină imaginație și de dorința de a-ți vedea visul împlinit.
-      <br>
-      <br>
-      Poate că te gândești că nu ai un spațiu potrivit pentru a face acest lucru, dar nici nu îți place ideea de a închiria un spațiu pe care să îl amenajezi ca un studio de înregistrări. Ei bine, îți spun că nu este nevoie să faci niciuna dintre aceste lucruri. Poți transforma o cameră din locuința ta într-un studio de înregistrări, cu costuri minime și cu rezultate deosebite.
-      <br>
-      Totuși, nici varianta actuală nu îți surâde prea mult, pentru că muncești mult mai mult pentru un sunet clar și curat, iar un studio de înregistrări ar face diferența pentru muzica ta.
-
-      Nu-ți face griji, întotdeauna există soluții.
-
-      În următoarele rânduri, am să îți spun de ce e mai convenabil să-ți amenajezi propriul tău home studio.
-
-      Am să îți dau și câteva recomandări pentru instrumentele și echipamentele de studio ideale pe care să le folosești, iar în final vei vedea că îți poți amenaja propriul studio de înregistrări, cu costuri reduse și rezultate excelente.
-
-      În final, cred că te vei convinge că este cea mai bună soluție pe care o poți lua în acest moment.
-
-      Nu spun că nu vei investi deloc, dar va fi o investiție acceptabilă și mult mai mică decât opțiunea de a închiria un spațiu dedicat pentru muzică.
-
-      Poți avea propriul tău home studio, la o cameră distanță de tine, iar sunetul va fi cel pe care îl cauți.
-
-      Vei avea puțin de lucru, dar îți promit că nu va fi un proces complex, care implică muncă grea și schimbări majore în structura camerei tale.
+      Dacă ai ajuns să citești aceste rânduri, înseamnă că ești un pasionat de
+      muzică și îți dorești să te vezi într-un studio de înregistrări, în care
+      muzica ta are o claritate aparte și devine un produs finit, pregătit să
+      ajungă la urechile publicului.
+      <br />
+      <br />
+      Ei bine, eu sunt aici pentru a-ți spune că
+      <b> poți avea propriul tău home studio, la tine acasă!</b> Cu un buget
+      minim și cu puțină creativitate, poți transforma o cameră din locuința ta
+      într-un studio de înregistrări.
+      <br />
+      <br />
+      În acest articol, îți voi prezenta pașii pe care trebuie să îi urmezi
+      pentru a-ți construi propriul studio de înregistrări, la tine acasă. Vei
+      vedea că nu este nevoie de investiții majore, ci doar de puțină imaginație
+      și de dorința de a-ți vedea visul împlinit.
+      <br />
+      <br />
+      Poate că te gândești că nu ai un spațiu potrivit pentru a face acest
+      lucru, dar nici nu îți place ideea de a închiria un spațiu pe care să îl
+      amenajezi ca un studio de înregistrări. Ei bine, îți spun că nu este
+      nevoie să faci niciuna dintre aceste lucruri. Poți transforma o cameră din
+      locuința ta într-un studio de înregistrări, cu costuri minime și cu
+      rezultate deosebite.
+      <br />
+      Totuși, nici varianta actuală nu îți surâde prea mult, pentru că muncești
+      mult mai mult pentru un sunet clar și curat, iar un studio de înregistrări
+      ar face diferența pentru muzica ta. Nu-ți face griji, întotdeauna există
+      soluții. În următoarele rânduri, am să îți spun de ce e mai convenabil
+      să-ți amenajezi propriul tău home studio. Am să îți dau și câteva
+      recomandări pentru instrumentele și echipamentele de studio ideale pe care
+      să le folosești, iar în final vei vedea că îți poți amenaja propriul
+      studio de înregistrări, cu costuri reduse și rezultate excelente. În
+      final, cred că te vei convinge că este cea mai bună soluție pe care o poți
+      lua în acest moment. Nu spun că nu vei investi deloc, dar va fi o
+      investiție acceptabilă și mult mai mică decât opțiunea de a închiria un
+      spațiu dedicat pentru muzică. Poți avea propriul tău home studio, la o
+      cameră distanță de tine, iar sunetul va fi cel pe care îl cauți. Vei avea
+      puțin de lucru, dar îți promit că nu va fi un proces complex, care implică
+      muncă grea și schimbări majore în structura camerei tale.
     </p>
 
-    <h2 class="text-center pa-4">Cum îți amenajezi propriul home studio audio?</h2>
+    <h2 class="text-center pa-4">
+      Cum îți amenajezi propriul home studio audio?
+    </h2>
 
     <v-img
       src="../assets/homestudio1.jpg"
       class="mx-auto"
-      max-width="800"></v-img>
+      max-width="800"
+    ></v-img>
 
     <p class="pa-4">
-      Scopul mixajului este sa echilibreze instrumentele si vocile astfel incat sa sune toate curat si sa nu se deranjeze reciproc. Cu cat ai mai multe instrumente muzicale, voci si sunete de mixat pe aceeasi portiune audio, cu atat mai greu este sa faci loc in mix fiecarui instrument. De aceea, este important sa ai un plan bine pus la punct inainte de a incepe mixajul.</p>
-      <br>
-      <br>
+      Scopul mixajului este sa echilibreze instrumentele si vocile astfel incat
+      sa sune toate curat si sa nu se deranjeze reciproc. Cu cat ai mai multe
+      instrumente muzicale, voci si sunete de mixat pe aceeasi portiune audio,
+      cu atat mai greu este sa faci loc in mix fiecarui instrument. De aceea,
+      este important sa ai un plan bine pus la punct inainte de a incepe
+      mixajul.
+    </p>
+    <br />
+    <br />
 
-      <!-- Timeline cu carduri --> 
-      <div>
+    <!-- Acordeoane cu info -->
+    <v-container>
+      <v-expansion-panels color="deep-orange-lighten-1" elevation="8">
+        <v-expansion-panel
+          title="Pasul 1 Gruparea percutiilor"
+          text="Gruparea percutiei intr-un subgrup. Am creat un canal nou de subgrup (sau bus, sau sub-mix), pe care am directionat fiecare din cele 7 canale rezervate percutiei. Acest pas este unul de organizare, care usureaza munca, nu este strict necesar. Mai tarziu, vei putea aplica efecte si modifica setarile intregului set de percutie, fara a fi nevoie sa modifici pe rand fiecare din cele 7 canale."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 2 Gruparea vocilor"
+          text="Gruparea vocilor intr-un nou subgrup. Din aceleasi motive pentru care am grupat si percutia in subgrup."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 3 Gruparea instrumentelor cu clape"
+          text="Gruparea clapelor intr-un nou subgrup, pentru ca este pe doua canale, si vreau sa operez cu efecte si modificari simultan pentru ambele canale."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 4 Gruparea instrumentelor in sub-mixuri"
+          text="Mixarea unui sub-mix. Incepem cu percutia. La acest pas trebuie sa pui pe solo intreaga percutie (deja se vede avantajul subgrupului, pentru ca daca pui pe solo grupul de percutie, se seteaza singure pe solo toate canalele de percutie), si sa alegi un loc din piesa unde se aud toate elementele sale simultan."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 5 Mixarea instrumentelor"
+          text="Mixarea instrumentelor. Incepem cu instrumentele cu clape. La acest pas trebuie sa pui pe solo intregul sub-mix de clape, si sa alegi un loc din piesa unde se aud toate elementele sale simultan.
+        Pune sa se repete acea portiune, si incepe sa faci mixajul: regleaza nivelurile fiecarui element de percutie in raport cu celalalte astfel incat sa sune cat mai uniform, sa se auda echilibrat toate. Incepe de exemplu cu toba mare, seteaz-o sa bata pana in -15 dB. Asta poate fi reperul, dupa care reglezi pe rand nivelurile celorlante elemente de percutie.
+        Si pentru ca este captata cu doua microfoane, vei avea un mic defazaj intre cele doua, care va trebui compensat. Poti vizualiza grafic daca cele doua canale de toba se suprapun perfect. In cazul meu a fost nevoie doar sa inversez faza de pe unul din cele doua canale.
+        Apoi urmeaza panoramarea celor doua tom-uri, stanga-dreapta, apoi panoramarea premierului si a cinelelor, inclusiv fuscinelului. Asculta percutia cu atentie si regleaza nivelul fiecarui canal in functie de reperul dat de toba mare.
+        Daca ai setat acel reper la -15 dB, ar trebui sa lucrezi la un volum destul de mic incat toata percutia sa ramana in limite rezonabile astfel incat indicatorul de nivel sa nu bata pe rosu. Nu uita ca elementele bateriei la mixare se insumeaza, ridicand nivelul total al percutiei, astfel ca la finalul mixarii percutiei, percutia singura sa ajunga pe indicatorul de nivel ceva mai sus decat reperul de la care ai plecat. Uneori mult mai sus datorita varfurilor date de premier sau toba mare.
+        In acest moment, fader-ul de pe canalul subgrupului de percutie ar trebui sa fie neatins, pe zero. La el vei umbla cand reglezi nivelul bateriei in functie de restul instrumentelor si vocilor. Un limitator sau/si compresor ar fi util pe acest canal de subgrup al percutiei, dar folosit cu atentie. De pe acum, trebuie sa te gandesti ca finalul mixului va urma sa treaca prin procesul de masterizare, si trebuie sa faci mixul astfel incat cel care va masteriza piesa sa aiba spatiu de lucru (headroom) in gama dinamica."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 5 Mixarea unui submix"
+          text="Mixarea unui alt sub-mix. In mod similar mixezi si vocile intre ele, luand ca reper vocea principala, pe care o reglezi sa nu treaca de -20dB, iar vocile secundare, sau de fundal, le reglezi in functie de cea principala. Un efect frumos este sa lasi vocea principala pe mijloc, iar cele secundare sa le panoramezi fiecare in directii opuse, undeva la 40-50%."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 6 Mixarea impreuna a submixurilor"
+          text="Acum ai doua subgrupuri mixate fiecare separat. E timpul sa le mixezi impreuna, adica percutia cu vocile. Pune cele doua subgrupuri pe solo, astfel incat sa auzi intreaga percutie si toate vocile. Seteaza percutia de exemplu sa nu treaca de -15dB, din fader-ul subgrupului, iar apoi mixeaza vocile cu percutia. Cele doua se insumeaza, si nivelul total iarasi creste, insa mai ai de adaugat si celalalte instrumente ramase, deci la final vei avea oricum un nivel mai ridicat per total."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 7 Mixarea chitarei bass cu bateria"
+          text="Mixarea chitarei bass cu bateria. Seteaza sa auzi doar subgrupul de baterie si canalul chitara bass, fara voci. Din nou, poti folosi ceva compresie sau/si egalizare, insa e bine sa folosesti doar daca este nevoie. Daca suna bine, lasa-le asa. Nu aplica efecte daca nu este necesar. 
+        Eu am dublat canalul de bass si pe cel dublat am aplicat o compresie dinamica mare, si usoare distorsiuni, pentru a-l face mai prezent, iar cele doua canale le-am mixat intre ele ca nivel, unul in functie de celalalt (sub -20 dB fiecare), dar si impreuna sa nu depaseasca -15 dB, eventual usor, pe alocuri.
 
-      </div>
-
-
+          Astfel setata chitara bass, am mixat-o cu grupul de percutie. Cand suna bine, da drumul si la subgrupul de voci sa il auzi, si verifica ca cele trei elemente (percutie, bass si voci) sa sune bine si uniform impreuna. Fa ajustarile necesare daca e cazul, si asculta din nou.
+        "
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 8 Mixarea instrumentelor de suflat"
+          text="Mixarea instrumentelor cu clape. Seteaza sa auzi doar subgrupul de clape si canalul de chitara bass, fara voci. Mixeaza-le intre ele, si apoi adauga-le la mixul general. Verifica ca suna bine si uniform impreuna cu celelalte elemente. Fa ajustarile necesare daca e cazul, si asculta din nou."
+        >
+        </v-expansion-panel>
+        <v-expansion-panel
+          title="Pasul 9 Mixarea finala"
+          text="Mixarea finala. Seteaza sa auzi toate elementele mixului, fara solo-uri. Mixeaza-le intre ele, si verifica ca suna bine si uniform impreuna. Fa ajustarile necesare daca e cazul, si asculta din nou. Daca suna bine, atunci ai terminat mixajul. Daca nu suna bine, fa ajustarile necesare si asculta din nou. Repeta pana suna bine."
+        >
+        </v-expansion-panel> </v-expansion-panels
+    ></v-container>
   </div>
 </template>
 <script>
-
 export default {
-  name: 'StudioView',
-  
+  name: "StudioView",
 };
-
 </script>
 <style lang=""></style>
