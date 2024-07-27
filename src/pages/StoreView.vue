236a6c2602c53e546f145473a3c1c489371d2f72
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-07-28 00:45:28 +0300

    StoreView - search filter improved

--- src/pages/StoreView.vue
+++ src/pages/StoreView.vue
@@ -2,14 +2,20 @@
   <div>
     <div>
       <v-container>
-
-
-        <v-card class="d-flex flex-wrap justify-center justify-space-evenly align-center  ga-4">
+        <v-card
+          class="d-flex flex-wrap justify-center justify-space-evenly align-center ga-4"
+        >
           <v-card-title>
             <h3>Produse;</h3>
           </v-card-title>
           <v-card-item>
-            <v-text-field width="300" label="Cauta produs" outlined dense></v-text-field>
+            <v-text-field
+              width="300"
+              label="Cauta produs"
+              outlined
+              dense
+              @input="searchProduct"
+            ></v-text-field>
           </v-card-item>
           <v-card-item>
             <v-select width="300" label="Sorteaza dupa" outlined dense>
@@ -19,21 +25,35 @@
             </v-select>
           </v-card-item>
           <v-card-item>
-            <v-btn append-icon="mdi-cart" color="purple" size="large" rounded="lg">Cos</v-btn>
+            <v-btn
+              append-icon="mdi-cart"
+              color="purple"
+              size="large"
+              rounded="lg"
+              >Cos</v-btn
+            >
           </v-card-item>
         </v-card>
-
-
       </v-container>
-      <div class="d-flex flex-wrap justify-space-evenly justify-center align-center align-self-center ma-2">
-        <CardProdus v-for="product in products" :key="product.id" :id="product.id" :image="product.image"
-          :title="product.title" :description="product.description" :price="product.price" :currency="product.currency"
-          :rating="product.rating.rate" :rate="product.rating.count" :count="product.rating.count"></CardProdus>
+      <div
+        class="d-flex flex-wrap justify-space-evenly justify-center align-center align-self-center ma-2"
+      >
+        <CardProdus
+          v-for="product in products"
+          :key="product.id"
+          :id="product.id"
+          :image="product.image"
+          :title="product.title"
+          :description="product.description"
+          :price="product.price"
+          :currency="product.currency"
+          :rating="product.rating.rate"
+          :rate="product.rating.count"
+          :count="product.rating.count"
+        ></CardProdus>
       </div>
     </div>
   </div>
-
-
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -62,6 +82,21 @@ export default defineComponent({
         },
       ],
     };
+  },
+  methods: {
+    searchProduct(event: Event) {
+      let target = event.target as HTMLInputElement;
+      let searchTerm = target.value.toLowerCase();
+      if (searchTerm == "") {
+        axios.get("https://fakestoreapi.com/products").then((response) => {
+          this.products = response.data;
+        });
+      } else {
+        this.products = this.products.filter((product) =>
+          product.title.toLowerCase().includes(searchTerm)
+        );
+      }
+    },
   },
   mounted() {
     axios.get("https://fakestoreapi.com/products").then((response) => {
