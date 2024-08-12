2a7ba4c7144c3f532b0cf62d1e151f48b2e6c3d9
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-08-12 19:38:30 +0300

    refactor: Update navbar links to use v-ripple directive

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -12,11 +12,27 @@
           <v-spacer></v-spacer>
 
           <v-toolbar-items>
-            <v-btn to="/home">Acasă</v-btn>
-            <v-btn to="/studio">Studio</v-btn>
-            <v-btn to="/listen-down">Listen & Download</v-btn>
-            <v-btn to="/store">Magazin</v-btn>
-            <v-btn to="/contact">Contact</v-btn>
+            <v-btn v-ripple="{ class: `text-deep-purple-accent-4` }" to="/home"
+              >Acasă</v-btn
+            >
+            <v-btn
+              v-ripple="{ class: `text-deep-purple-accent-4` }"
+              to="/studio"
+              >Studio</v-btn
+            >
+            <v-btn
+              v-ripple="{ class: `text-deep-purple-accent-4` }"
+              to="/listen-down"
+              >Listen & Download</v-btn
+            >
+            <v-btn v-ripple="{ class: `text-deep-purple-accent-4` }" to="/store"
+              >Magazin</v-btn
+            >
+            <v-btn
+              v-ripple="{ class: `text-deep-purple-accent-4` }"
+              to="/contact"
+              >Contact</v-btn
+            >
 
             <v-divider
               class="mx-2"
@@ -25,41 +41,85 @@
               inset
               vertical
             ></v-divider>
-            <SocialBtn class="d-flex justify-center pa-2"/>
+            <SocialBtn class="d-flex justify-center pa-2" />
           </v-toolbar-items>
         </template>
-</v-toolbar>
-</v-app-bar>
-<v-navigation-drawer v-if="$vuetify.display.mdAndDown" v-model="drawer"
-  :location="$vuetify.display.mobile ? 'top' : undefined" theme="dark" floating="auto" app>
-  <v-list :items="items">
-    <template v-slot:prepend> </template>
-    <v-list-item v-for="(item, i) in items" :key="i" @click="drawer = false" :to="item.link">
-      <v-list-item-content>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
-      </v-list-item-content>
+      </v-toolbar>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-if="$vuetify.display.mdAndDown"
+      v-model="drawer"
+      :location="$vuetify.display.mobile ? 'top' : undefined"
+      theme="dark"
+      floating="auto"
+      app
+    >
+      <v-list :items="items">
+        <template v-slot:prepend> </template>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          @click="drawer = false"
+          :to="item.link"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-    </v-list-item>
-
-    <v-container class="text-center">
-      <v-btn :ripple="false" variant="plain" rounded="0" icon="mdi-facebook"
-        href="https://www.facebook.com/dan.surlaru.promusic" target="_blank"></v-btn>
-      <v-btn :ripple="false" variant="plain" rounded="0" icon="mdi-instagram" href="https://instagram.com/danypromusic"
-        target="_blank"></v-btn>
-      <v-btn :ripple="false" variant="plain" rounded="0" icon="mdi-linkedin"
-        href="https://www.linkedin.com/in/dan-surlaru-promusic-418a072a3/" target="_blank"></v-btn>
-      <v-btn :ripple="false" variant="plain" rounded="0" icon="mdi-youtube"
-        href="https://www.youtube.com/@dansurlaru/featured" target="_blank"></v-btn>
-      <v-btn :ripple="false" variant="plain" rounded="0" icon="mdi-spotify"
-        href="https://open.spotify.com/artist/4viXypcCjfNp3LrDQXdzbf?go=1&sp_cid=e7e7564b5f5e7489b13d62256216a2b7&utm_source=embed_player_p&utm_medium=desktop&nd=1&dlsi=7690dae220434f38"
-        target="_blank"></v-btn>
-      <v-btn :ripple="false" variant="plain" rounded="0" icon="mdi-apple"
-        href="https://music.apple.com/md/artist/dan-surlaru/1449898549" target="_blank"></v-btn>
-    </v-container>
-
-  </v-list>
-</v-navigation-drawer>
-</v-layout>
+        <v-container class="text-center">
+          <v-btn
+            :ripple="false"
+            variant="plain"
+            rounded="0"
+            icon="mdi-facebook"
+            href="https://www.facebook.com/dan.surlaru.promusic"
+            target="_blank"
+          ></v-btn>
+          <v-btn
+            :ripple="false"
+            variant="plain"
+            rounded="0"
+            icon="mdi-instagram"
+            href="https://instagram.com/danypromusic"
+            target="_blank"
+          ></v-btn>
+          <v-btn
+            :ripple="false"
+            variant="plain"
+            rounded="0"
+            icon="mdi-linkedin"
+            href="https://www.linkedin.com/in/dan-surlaru-promusic-418a072a3/"
+            target="_blank"
+          ></v-btn>
+          <v-btn
+            :ripple="false"
+            variant="plain"
+            rounded="0"
+            icon="mdi-youtube"
+            href="https://www.youtube.com/@dansurlaru/featured"
+            target="_blank"
+          ></v-btn>
+          <v-btn
+            :ripple="false"
+            variant="plain"
+            rounded="0"
+            icon="mdi-spotify"
+            href="https://open.spotify.com/artist/4viXypcCjfNp3LrDQXdzbf?go=1&sp_cid=e7e7564b5f5e7489b13d62256216a2b7&utm_source=embed_player_p&utm_medium=desktop&nd=1&dlsi=7690dae220434f38"
+            target="_blank"
+          ></v-btn>
+          <v-btn
+            :ripple="false"
+            variant="plain"
+            rounded="0"
+            icon="mdi-apple"
+            href="https://music.apple.com/md/artist/dan-surlaru/1449898549"
+            target="_blank"
+          ></v-btn>
+        </v-container>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 </template>
 <script>
 import { defineComponent } from "vue";
