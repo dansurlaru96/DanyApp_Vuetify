6c15e04f187e06d64fe7570593c00a7bfa4f393c
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-08-13 17:27:17 +0300

    refactor: Update image source URLs and fix typos in CardInfo.vue

--- src/components/CardInfo.vue
+++ src/components/CardInfo.vue
@@ -21,7 +21,7 @@
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "CardInfo",
-  defineProps: {
+  props: {
     image: String,
     subject: String,
     text: String,
